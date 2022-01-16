956fb6e6e21261f7ea271609472cf2a8511963ef
Paul Elden <paulelden11@gmail.com>
2022-01-16 16:12:59 -0500

    Added animation for the tiles to make it more interesting. Also added some light formatting and styling to index

--- scripts/index.js
+++ scripts/index.js
@@ -4,34 +4,43 @@ grid.style.alignItems = 'left';
 grid.style.justifyContent = 'space-evenly';
 grid.style.flexWrap = 'wrap';
 
-const newGrid = document.getElementById('newgrid');
-newGrid.addEventListener('click', () => {
-    let gr = document.getElementById('grid');
-    let child = gr.lastElementChild;
-    while (child) {
+const newGrid = document.getElementById('newgrid'); // this is the code for the button
+newGrid.addEventListener('click', () => {           
+    let gr = document.getElementById('grid');   // the contents of this function will 
+    let child = gr.lastElementChild;            // delete the children of the grid/container
+    while (child) {                                 
         gr.removeChild(child);
         child = gr.lastElementChild;
     }
-    console.log('triggered');
-    let userInput = parseInt(prompt("How many tiles?"));
+    let userInput = parseInt(prompt("How many tiles?")); // then prompt for new tilecount
     spawnTiles(userInput);
 });
 
 // functions
 
 function spawnTile(){
-    const tile = document.createElement('div');
-    tile.classList.add('tileStyle');
-    grid.appendChild(tile);
-    tile.addEventListener('mouseover', () => {
-        tile.classList.add('tileStyleActive')
+    const keyframe = [  // this keyframe is used to animate the tile on mouse exit
+        { backgroundColor: 'black'},
+        { backgroundColor: 'white'}
+    ];
+    const tile = document.createElement('div'); // create tile in memory
+    tile.classList.add('tileStyle');            // add basic style
+    grid.appendChild(tile);                     // instantiate tile in DOM
+
+    tile.addEventListener('mouseenter', () => { // on mouse enter apply active style,
+        tile.classList.add('tileStyleActive')   // changing color
+    });
+
+    tile.addEventListener('mouseout', () => {   // on mouse exit animate tile back to 
+        tile.animate(keyframe, 1000);           // original color, remove active style
+        tile.classList.remove('tileStyleActive');
     });
     
 }
 
 function spawnTiles(n) {    // where n = number of tiles 
     for (let i = 0; i < n; i++){
-      spawnTile(); // setTimeout is not working here for some reason
+      spawnTile(); 
     }
 }
 

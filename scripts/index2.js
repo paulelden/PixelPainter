61462252dae416b71229164279fc4b433e7cf576
Paul Elden <paulelden11@gmail.com>
2022-01-29 17:42:05 -0500

    Added scripting for canvas creation and the clear canvas button. Also added listener for tile coloring

--- scripts/index2.js
+++ scripts/index2.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById('canvas');
 const blackButton = document.getElementById('blackButton');
 const colorSelector = document.getElementById('colorSelector');
 const rainbowMode = document.getElementById('rainbowMode');
-const clearCanvas = document.getElementById('clearCanvas');
+const clearButton = document.getElementById('clearButton');
 const sizeinput = document.getElementById('sizeInput');
 const newCanvas = document.getElementById('newCanvas');
 let selectedColor = document.getElementById('colorSelector').value;
@@ -15,18 +15,43 @@ let canvasSize = document.getElementById('sizeInput').value;
 blackButton.addEventListener('click', function() {
     selectedColor = '#000000';
     colorSelector.value = '#000000';
+    return selectedColor;
 });
 
-colorSelector.addEventListener('change', function(e) {
+colorSelector.addEventListener('change', function() {
     selectedColor = colorSelector.value;
     console.log(selectedColor);
     return selectedColor;
 });
 
+clearButton.addEventListener('click', clearCanvas);
+
 
 // functions
 
+function generateCanvas(canvasSize) {
+    canvas.style.gridTemplateColumns = `repeat(${canvasSize}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${canvasSize}, 1fr)`;
 
+    for (let i = 0; i < canvasSize**2; i++) {
+        const tile = document.createElement('div');
+        tile.classList.add('newTile');
+        canvas.appendChild(tile);
+        tile.addEventListener('mouseover', function (e) {
+            tile.style.backgroundColor = selectedColor;
+        });
+    }
+
+}
+
+function updateColor(selectedColor) {
+    tile.style.backgroundColor = selectedColor;
+}
+
+function clearCanvas() {
+    let tiles = canvas.querySelectorAll('div');
+    tiles.forEach(tile => tile.style.backgroundColor = '#ffffff')
+} 
 
 // main
-canvasSize = '12';
+generateCanvas(10);

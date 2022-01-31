10bddb1162d2303ab5c0d0995341d765628c1315
Paul Elden <paulelden11@gmail.com>
2022-01-30 22:47:11 -0500

    Merged the pixel painter branch into main and deleted old 1.0 files, renamed 2.0 files.

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,105 @@
-const grid = document.getElementById("grid"); // assigning basic properties to grid
-grid.style.display = 'flex'; 
-grid.style.alignItems = 'left';
-grid.style.justifyContent = 'space-evenly';
-grid.style.flexWrap = 'wrap';
+// variables
 
-const newGrid = document.getElementById('newgrid'); // this is the code for the button
-newGrid.addEventListener('click', () => {           
-    let gr = document.getElementById('grid');   // the contents of this function will 
-    let child = gr.lastElementChild;            // delete the children of the grid/container
-    while (child) {                                 
-        gr.removeChild(child);
-        child = gr.lastElementChild;
-    }
-    let userInput = parseInt(prompt("How many tiles?")); // then prompt for new tilecount
-    spawnTiles(userInput);
+const canvas = document.getElementById('canvas');
+const blackButton = document.getElementById('blackButton');
+const colorSelector = document.getElementById('colorSelector');
+const toggleRainbowButton = document.getElementById('toggleRainbowButton');
+const toggleGridButton = document.getElementById('toggleGridButton');
+const clearButton = document.getElementById('clearButton');
+const sizeinput = document.getElementById('sizeInput');
+const newCanvas = document.getElementById('newCanvas');
+
+
+let selectedColor = document.getElementById('colorSelector').value;
+let canvasSize = document.getElementById('sizeInput').value;
+let rainbow = false;
+
+// listeners 
+
+blackButton.addEventListener('click', function() {
+    selectedColor = '#000000';
+    colorSelector.value = '#000000';
+    return selectedColor;
 });
+
+colorSelector.addEventListener('change', function() {
+    selectedColor = colorSelector.value;
+    console.log(selectedColor);
+    return selectedColor;
+});
+
+toggleRainbowButton.addEventListener('click', toggleRainbow);
+
+toggleGridButton.addEventListener('click', toggleGrid);
+
+clearButton.addEventListener('click', clearCanvas);
+
+newCanvas.addEventListener('click', function(){
+    generateCanvas(sizeinput.value);
+})
 
 // functions
 
-function spawnTile(){
-    const keyframe = [  // this keyframe is used to animate the tile on mouse exit
-        { backgroundColor: 'black'},
-        { backgroundColor: 'white'}
-    ];
-    const tile = document.createElement('div'); // create tile in memory
-    tile.classList.add('tileStyle');            // add basic style
-    grid.appendChild(tile);                     // instantiate tile in DOM
+function generateCanvas(canvasSize) {
+    canvas.style.gridTemplateColumns = `repeat(${canvasSize}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${canvasSize}, 1fr)`;
+    if (canvas.hasChildNodes){
+        let tile = canvas.firstElementChild;
+        while (tile) {
+            canvas.removeChild(tile);
+            tile = canvas.firstElementChild;
+        }
+    }
 
-    tile.addEventListener('mouseenter', () => { // on mouse enter apply active style,
-        tile.classList.add('tileStyleActive')   // changing color
-    });
+    for (let i = 0; i < canvasSize**2; i++) {
+        const tile = document.createElement('div');
+        tile.classList.add('grid');
+        tile.style.backgroundColor = '#ffffff';
+        canvas.appendChild(tile);
+        tile.addEventListener('mouseover', function (e) {
+            if (rainbow == true){
+                tile.style.backgroundColor = randomColor();
+            }
+            else {
+                tile.style.backgroundColor = selectedColor;
+            }
+        });
+    }
 
-    tile.addEventListener('mouseout', () => {   // on mouse exit animate tile back to 
-        tile.animate(keyframe, 1000);           // original color, remove active style
-        tile.classList.remove('tileStyleActive');
-    });
-    
 }
 
-function spawnTiles(n) {    // where n = number of tiles 
-    for (let i = 0; i < n; i++){
-      spawnTile(); 
+function randomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return "#" + randomColor;
+}
+
+function updateColor(selectedColor) {
+    tile.style.backgroundColor = selectedColor;
+}
+
+function toggleRainbow() {
+    if (rainbow == true) {
+        rainbow = false;
+    }
+    else {
+        rainbow = true;
     }
 }
 
+function toggleGrid() {
+    let tiles = canvas.querySelectorAll('div');
+    if (tiles[0].classList.contains('grid')) {
+        tiles.forEach(tile => tile.classList.remove('grid'));
+    }
+    else {
+        tiles.forEach(tile => tile.classList.add('grid'));
+    }
+}
+
+function clearCanvas() {
+    let tiles = canvas.querySelectorAll('div');
+    tiles.forEach(tile => tile.style.backgroundColor = '#ffffff')
+} 
 
 // main
-let howMany = parseInt(prompt("How many tiles?"));
-if (howMany > 100) { 
-    howMany = 100
-}
-else {
-    // pass 
-}
-spawnTiles(howMany);
+generateCanvas(10);
